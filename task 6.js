a86981df81ddeea1da06cfc9445429028481950d
Aashiq <sheriffxr@gmail.com>
2026-05-15 21:53:09 +0530

    first commit

---
+++ task 6.js
@@ -0,0 +1,51 @@
+
+
+
+ // 1. Student Attendance System
+
+
+// const presentStudents = ["mothish","akash","aashiq"];
+// const absentStudents =["sultan","surendar"];
+// let attendenceList = [...presentStudents, ...absentStudents];
+// attendenceList.push("arun");
+// console.log(attendenceList);
+
+
+ // 2. E-Commerce Cart
+
+
+// const mobile = { brand: "Nothing", price: 55000};
+// const charger = { type: "Fast Charger", warranty: "3 Year"};
+// const productDetails = {...mobile, ...charger, deliveryDate: "19-Dec-2029"};
+// console.log(productDetails);
+
+
+// 3. Food Delivery App
+
+// function orderFood(...items) { console.log("Total items ordered:", items.length); console.log("First item:", items[0]); console.log("Last item:", items[items.length - 1]); }
+// orderFood("Biryani", "Chicken lasangya", "Fried noodles", "fruit salad");
+
+
+
+// 4. Employee Salary Filter
+
+// const employees = [
+//     { name: "gokula", salary: 45000 },
+//     { name: "meega", salary: 60000 },
+//     { name: "yuvashree", salary: 75000 },
+//     { name: "vasudharani", salary: 40000 }
+// ];
+// const highSalaryEmployees = employees.filter(function(emp) {
+//     return emp.salary > 50000;
+// });
+// console.log(highSalaryEmployees);
+
+
+
+// 5. Online Game Score Board
+
+// const scores = [500, 600, 750, 150, 200];
+// const totalScore = scores.reduce((total, score) => {
+//     return total + score;
+// }, 0);
+// console.log("Total Score:", totalScore);
